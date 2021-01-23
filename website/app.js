52214d5c07f8b3bc2c64df9a20605c58d2dd12e0
shuxter <simon.huxter@gmail.com>
2021-01-23 19:39:39 -0300

    Added in NAV functionality with B5 and added in two options(still basic) for the search bar functionality

--- website/app.js
+++ website/app.js
@@ -1,37 +1,42 @@
 const { static } = require("express");
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const app = express();
+const PORT = 3000;
 
-app.listen(3000);
-
+//Middleware
 const staticFolder = path.resolve(__dirname, "./public");
 app.use(express.static(staticFolder));
 
+//Router
+app.listen(PORT, () => {
+	console.log(`The server is running on ${PORT}`);
+});
+
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+	res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 app.get("/vehicle/item", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/productVehicle.html"));
+	res.sendFile(path.resolve(__dirname, "./views/productVehicle.html"));
 });
 app.get("/product/item", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/productPart.html"));
+	res.sendFile(path.resolve(__dirname, "./views/productPart.html"));
 });
 app.get("/vehicle/add", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/addVehicle.html"));
+	res.sendFile(path.resolve(__dirname, "./views/addVehicle.html"));
 });
 app.get("/product/add", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/addProduct.html"));
+	res.sendFile(path.resolve(__dirname, "./views/addProduct.html"));
 });
 app.get("/cart", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/cart.html"));
+	res.sendFile(path.resolve(__dirname, "./views/cart.html"));
 });
 app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+	res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
 app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+	res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
 app.get("/search", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/search.html"));
+	res.sendFile(path.resolve(__dirname, "./views/search.html"));
 });
